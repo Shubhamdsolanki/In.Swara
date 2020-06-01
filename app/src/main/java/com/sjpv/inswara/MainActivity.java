3f30d9bf551984d1b71c69847bdbe6a94c784833
Vinayak D Madiwalar <vinayakmadiwalar9@gmail.com>
2020-06-01 18:58:04 +0530

    Initial commit

---
+++ app/src/main/java/com/sjpv/inswara/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sjpv.inswara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
